9f8124033ed20e1c67781bdecd4c02e467253be2
Yen Choi <yenchoi010@gmail.com>
2022-04-28 23:48:41 +0800

    feat: collapsed menu

---
+++ main.js
@@ -0,0 +1,11 @@
+// navOpened = document.getElementsByClassName("navOpened");
+
+
+// window.onload = function() {
+//     function openNav() {
+//         navOpened = document.getElementsByClassName("navOpened");
+//         const list = navOpened.classList;
+//         list.remove("hidden");
+//     }
+    
+// }
