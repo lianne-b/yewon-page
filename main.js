ef6ee90aa9ffd1f82857aa8ddeccd2b43ca3c946
Yen Choi <yenchoi010@gmail.com>
2022-05-02 22:41:37 +0800

    feat: onclick video full screen

--- main.js
+++ main.js
@@ -1,7 +1,29 @@
+///// nav
+
 navOpened = document.getElementsByClassName("navOpened");
 
 
 function openNav() {
     let nav = document.querySelector(".navOpened");
     nav.classList.toggle("hidden");
+}
+
+
+///// reel
+
+function fullScreen() {
+    const reel = document.getElementsByTagName("video")[0];
+    if (reel.requestFullscreen) {
+        reel.requestFullscreen();
+    }else if (reel.mozRequestFullScreen) {
+        reel.mozRequestFullScreen();
+    }else if (reel.webkitRequestFullscreen) {
+        reel.webkitRequestFullscreen();
+    }else if (reel.msRequestFullscreen) { 
+        reel.msRequestFullscreen();
+    }else{
+        alert("Full screen not supported");
+        return;
+    }
+    reel.play();
 }
